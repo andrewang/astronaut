7128b3185823a4a297faa159164344028f7caaa4
Pedro Marín Ramos <pmarinr@gmail.com>
2014-02-21 16:58:44 +0100

    Animations rulez

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,15 +9,49 @@ public class PlayerController : MonoBehaviour {
 	
 	public float moveForce = 365f;            
 	public float maxSpeed = 5f;            
-	public float jumpForce = 1000f;    
+	public float jumpForce = 1000f; 
+	public float verticalv = 0;
+	private bool grounded = false; 
 
-	private bool grounded = false;            
+	private float localScalex = 1f;
+
+	Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+		localScalex = transform.localScale.x;
+
+
+	}
+
 	
 	void Update()
 	{
 		
-		if(Input.GetButtonDown("Jump"))
+		if (Input.GetButtonDown ("Jump")) {
 			jump = true;
+			anim.SetBool("jump",true);
+
+		}
+
+		if (rigidbody2D.velocity.y == 0f) {
+						anim.SetBool ("ground", true);
+		} else {
+				anim.SetBool("ground",false);
+		}
+
+		if (rigidbody2D.velocity.x > 0f) {
+			anim.SetFloat ("velocity", Mathf.Abs(rigidbody2D.velocity.x));
+			Vector3 localx = new Vector3(localScalex ,transform.localScale.y,transform.localScale.z );
+			transform.localScale = localx ;
+		} else if (rigidbody2D.velocity.x < 0f) {
+			Vector3 localx = new Vector3(localScalex * -1,transform.localScale.y,transform.localScale.z );
+				transform.localScale = localx ;
+				anim.SetFloat ("velocity", Mathf.Abs(rigidbody2D.velocity.x));
+				
+		}else{
+				anim.SetFloat ("velocity", 0);
+		}
 		
 	}
 	
@@ -37,6 +71,7 @@ public class PlayerController : MonoBehaviour {
 		{    
 			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
 			jump = false;
+			anim.SetBool("jump",false);
 		}
 		
 		
