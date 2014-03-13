49ce6f65d59e5cc2abdc63ca9ab307818d123abd
pililopez <pililoji@hotmail.com>
2014-03-13 12:04:28 +0100

    gamecontrol_pilar

---
+++ Assets/Scripts/pilar/pilar_CoingScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pilar_CoingUpScript : MonoBehaviour {
+	public AudioClip LevelUp;
+	public static bool dead;
+	float resetTimer = 0 ; 
+	public int puntos =1;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D col) {
+		if (Time.time > resetTimer && !GameControl.dead){
+			AudioSource.PlayClipAtPoint(LevelUp, transform.position);
+		    resetTimer = Time.time + 3;
+			GameControl.score = GameControl.score + puntos;
+			//Debug.Log ("LevelUp;" + GameControl.score);
+			Destroy (gameObject);
+		}
+
+	}
+
+}
