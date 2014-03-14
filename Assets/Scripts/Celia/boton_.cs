1653ed0becb9825e1a3af6d978cb97e0e3709105
CeliaV <celiavilches94@gmail.com>
2014-03-14 19:30:44 +0100

    Celia_Viernes

---
+++ Assets/Scripts/Celia/boton_.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boton_ : MonoBehaviour {
+	public GameObject boton;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void OnTriggerEnter2D(Collider2D other){
+				var exp = Instantiate (boton, transform.position, transform.rotation);
+				Destroy (gameObject);
+		}
+} 
---
+++ Assets/Scripts/Celia/botoncontroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class botoncontroller : MonoBehaviour {
+	
+	public float delay = 3f; 
+	private float reactiveTime = 0f;
+	private bool pulsado = false;
+	Animator anim;
+	//public GameObject plataforma;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((reactiveTime < Time.time) && pulsado){
+			anim.SetBool ("pulsado", false);
+			pulsado = true;
+		}
+	}
+	
+
+}
