3efa3e3c8c581aa22a27ce462fae923ca2badcb6
pililopez <pililoji@hotmail.com>
2014-03-14 16:44:40 +0100

    pablo t

---
+++ Assets/Scripts/Pablom/Pablo_TeleporterScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pablo_TeleporterScript : MonoBehaviour {
+
+	public GameObject T2;
+	private Vector3 destino;
+	// Use this for initialization
+	void Start () {
+		destino = T2.transform.position;
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		
+		if (other.transform.tag == "Player"){
+			other.transform.position = destino;
+			other.rigidbody2D.velocity = Vector3.zero;
+		}
+	}
+}
