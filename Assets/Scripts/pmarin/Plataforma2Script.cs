862317d43bd8e8b4ea65b8a989c20bf4dde63357
pmarinr <pmarinr@gmail.com>
2014-03-14 17:45:04 +0100

    plataforma2

--- Assets/Scripts/pmarin/Plataforma2Script.cs
+++ Assets/Scripts/pmarin/Plataforma2Script.cs
@@ -3,22 +3,56 @@ using System.Collections;
 
 public class Plataforma2Script : MonoBehaviour {
 
-	private GameObject P1;
-	private GameObject P2;
+	private Transform P1;
+
 	public bool pause = true;
 	public float waitTime = 0; 
 	public bool auto =false;
+	public float speed = 1f;
+
+	private Vector3 origen;
+	private Vector3 destino;
+	private float startTime;
+	public float distancia;
+	public bool adestino = true;
+	public float distCovered;
 	// Use this for initialization
 	void Start () {
-		//origen = transform.position.y;
-		//destino = transform.position.y + ejeY;
+		P1 = transform.Find("P1");
+
+		origen = transform.position;
+		destino = P1.position;
+		startTime = Time.time;
+		distancia = Vector3.Distance(origen, destino);
 	}
 	// Update is called once per frame
 	void Update () {
+		distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / distancia;
 
+		if(adestino && !pause){
+			transform.position = Vector3.Lerp(origen, destino, fracJourney);
+			if(transform.position == destino){
+				adestino = false;
+				distCovered = 0;
+				startTime = Time.time;
+				if(!auto) pause = true;
+			}
+
+		}else if(!adestino && !pause){
+			transform.position = Vector3.Lerp(destino, origen, fracJourney);
+			if(transform.position == origen){
+				adestino = true;
+				distCovered = 0;
+				startTime = Time.time;
+				if(!auto) pause = true;
+			}
+
+		}
 	}
 
   void activar(){
 		pause = false;
+		startTime = Time.time;
 	}
 }
