82b176e01e08c81e8c9e6ceffa34ba4686d8944f
pililopez <piloloji@hotmail.com>
2014-03-23 20:30:23 +0100

    Domingo noche

---
+++ Assets/Scripts/pilar/pilar_CorazonScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pilar_CorazonScript : MonoBehaviour {
+	public AudioClip Corazon;
+	public static bool dead;
+	float resetTimer = 0 ; 
+	public int vida =50;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D col) {
+		if (Time.time > resetTimer && !GameControl.dead){
+			AudioSource.PlayClipAtPoint(Corazon, transform.position);
+		    resetTimer = Time.time + 3;
+			GameControl.score = GameControl.live + vida;
+			//Debug.Log ("LevelUp;" + GameControl.score);
+			Destroy (gameObject);
+		}
+
+	}
+
+}
